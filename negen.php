a298212300a7fddbdea5e991de911c1734ed7c7e
mchennoufi1 <70711395+mchennoufi1@users.noreply.github.com>
2021-10-06 20:05:11 +0200

    Update negen.php

--- negen.php
+++ negen.php
@@ -23,7 +23,7 @@
         <h3>Hoofdstuk 9</h3>
         <p><a href="opdracht1.php">Opdracht 1</a></p>
         <p><a href="opdracht3.php">Opdracht 3</a></p>
-        <p><a href="opdracht4.php">Opdracht 4</a></p>
+        <p><a href="opdracht4.php">Opdracht 4 en 5</a></p>
         <p><a href="opdracht6.php">Opdracht 6</a></p>
         <p><a href="opdracht7.php">Opdracht 7</a></p>
     </body>
